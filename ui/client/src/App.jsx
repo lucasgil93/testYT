c4b0e13e53fd2ec45106bfa19eb86e3a51ff5ba1
lucasgil93 <lucasgildominguez@gmail.com>
2024-05-20 17:30:28 +0200

    cleanup2

--- ui/client/src/App.jsx
+++ ui/client/src/App.jsx
@@ -11,50 +11,10 @@ import Landing from "./components/Landing"
 
 
 function App() {
-  /*const [count, setCount] = useState(0)
-  const [data, setData] = useState([])
 
-
-  let API_URL = "http://localhost:5038/";
-
-  useEffect(() => {
-    const getJSON = async () => {
-      try {
-        const response = await fetch(API_URL + 'api/project/GetMeals');
-        const responseData = await response.json();
-        setData(responseData);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getJSON();
-  }, []);
-
-  console.log(data)
-
-  function createCardGame(element) {
-    let title = element.name;
-    let subTitle = element.area;
-    let text = element.category;
-    let img = element.image;
-
-
-    return <CardGame
-      title={title}
-      subTitle={subTitle}
-      text={text}
-      img={img}
-    />;
-  }
-*/
   return (
     <>
-      {/*<MenuTop />
-      <section>
-        {data.map((e) => createCardGame(e))}
-      </section>
-*/}
-        <Landing></Landing>
+      <Landing></Landing>
     </>
   )
 }
