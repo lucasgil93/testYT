c30de52fa4593373087e6f23ebbb0d480d7c6ab0
lucasgil93 <lucasgildominguez@gmail.com>
2024-05-20 14:03:08 +0200

    Cambios visuales a menuitem

--- ui/client/src/components/MenuItem.jsx
+++ ui/client/src/components/MenuItem.jsx
@@ -14,15 +14,17 @@ export default function MenuItem(props) {
  ^*/
     return (
 
-        <div className="menuItem flex justify-start items-left bg-orange-50 m-7 p-4 border-red-500 border-2 rounded-lg shadow-lg">
+        <div className="menuItem bg-orange-50 m-7 p-2 border-red-500 border-2 rounded-lg shadow-lg">
             <Card name={props.name} category={props.category}>
-                <div className="p-4">
-                    <h2 className="text-3xl font-bold text-gray-900 mb-2">{props.name}</h2>
-                    <p className="text-gray-700 text-xl mb-4">{props.ingredients.map(ingredient => ingredient.toUpperCase()).join(', ')}</p>
-                    <h2 className="text-2xl font-semibold text-gray-800">{props.price}<span>€</span></h2>
+                <div className="p-2">
+                    <div className='flex flex-row items-center justify-between'>
+                    <h2 className="text-2xl font-bold text-gray-900 ">{props.name}</h2>
+                    <h2 className="text-2xl font-semibold text-red-800">{props.price}<span>€</span></h2>
+                    </div>
+                    <p className="text-gray-700 text-sm ml-20">{props.ingredients.map(ingredient => ingredient.toUpperCase()).join(', ')}</p>
                     {props.allergens && props.allergens.length > 0 && (
-                        <div className="mt-4">
-                            <p className="allergens text-md text-red-600">Allergens: {props.allergens.join(', ')}</p>
+                        <div className="">
+                            <p className="allergens text-sm ml-20 text-red-600">Allergens: {props.allergens.join(', ')}</p>
                         </div>
 
                     )}
